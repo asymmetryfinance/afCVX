76df9d9600916fc902b4e32e3664dfb9426b89e9
0xIryna <ihubernska@gmail.com>
2024-05-13 00:01:17 -0700

    ⚡ [G-5] use storage instead of memory in getRequestedUnlocks()

--- src/strategies/CLeverCVXStrategy.sol
+++ src/strategies/CLeverCVXStrategy.sol
@@ -111,7 +111,7 @@ contract CleverCvxStrategy is ICleverCvxStrategy, TrackedAllowances, Ownable, UU
     }
 
     function getRequestedUnlocks(address account) external view returns (UnlockRequest[] memory unlocks) {
-        UnlockRequest[] memory accountUnlocks = requestedUnlocks[account].unlocks;
+        UnlockRequest[] storage accountUnlocks = requestedUnlocks[account].unlocks;
         uint256 nextUnlockIndex = requestedUnlocks[account].nextUnlockIndex;
         uint256 unlocksLength = accountUnlocks.length;
         unlocks = new UnlockRequest[](unlocksLength - nextUnlockIndex);
